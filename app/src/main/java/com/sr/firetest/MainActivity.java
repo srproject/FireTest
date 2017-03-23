047d7e1e115eb2b883cc121c65dcc15ecac620de
sr <SR>
2017-03-23 04:07:43 +0200

    login

--- app/src/main/java/com/sr/firetest/MainActivity.java
+++ app/src/main/java/com/sr/firetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.sr.firetest;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,15 +33,21 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private EditText ename,eage;
-    private Button eabu;
+    private Button eabu ;
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference db;
     FirebaseHelper helper;
     CustomAdapter adapter;
     ListView lv;
-    EditText nameEditTxt, propTxt, descTxt;
+    EditText nameEditTxt, propTxt, descTxt,emailEditText,passed;
     Context context;
+
+    String un=null;
+
+    private FirebaseAuth firebaseAuth;
+
+
 
     private SwipeRefreshLayout swipeRefreshLayout;
 
@@ -63,6 +75,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginDialog();
+
 
         lv = (ListView) findViewById(R.id.lv);
         //INITIALIZE FIREBASE DB
@@ -112,6 +129,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton fabl = (FloatingActionButton) findViewById(R.id.fabl);
+        fabl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginDialog();
+            }
+        });
+
 
 
 
@@ -145,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
         d.setTitle("Send To Firebase");
         d.setContentView(R.layout.inputdialog);
         nameEditTxt = (EditText) d.findViewById(R.id.nameEditText);
-        propTxt = (EditText) d.findViewById(R.id.propellantEditText);
+        nameEditTxt.setText(un);
         descTxt = (EditText) d.findViewById(R.id.descEditText);
         Button saveBtn = (Button) d.findViewById(R.id.saveBtn);
         //SAVE
@@ -154,21 +179,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //GET DATA
                 String name = nameEditTxt.getText().toString();
-                String propellant = propTxt.getText().toString();
-                String desc = descTxt.getText().toString();
+                 String desc = descTxt.getText().toString();
                 //SET DATA
                 Spacecraft s = new Spacecraft();
                 s.setName(name);
-                s.setPropellant(propellant);
-                s.setDescription(desc);
+                 s.setDescription(desc);
                 //SIMPLE VALIDATION
                 if (name != null && name.length() > 0) {
                     //THEN SAVE
                     if (helper.save(s)) {
                         //IF SAVED CLEAR EDITXT
-                        nameEditTxt.setText("");
-                        propTxt.setText("");
-                        descTxt.setText("");
+                       // nameEditTxt.setText("");
+                         descTxt.setText("");
                         adapter = new CustomAdapter(MainActivity.this, helper.retrieve());
                         lv.setAdapter(adapter);
                     }
@@ -181,6 +203,76 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void loginDialog() {
+        final Dialog d = new Dialog(this);
+        d.setTitle("Signin/Signup To Firebase");
+        d.setContentView(R.layout.logindialog);
+        emailEditText = (EditText) d.findViewById(R.id.emailEditText);
+        passed = (EditText) d.findViewById(R.id.passed);
+        Button lgBtn = (Button) d.findViewById(R.id.lgBtn);
+        Button regBtn = (Button) d.findViewById(R.id.regBtn);
+        //SAVE
+
+        lgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passed.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("SR", "Entered onComplete");
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(),"Error - "+task.getException().getMessage().toString(),Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+
+                                    Toast.makeText(getApplicationContext(),"Yes Ok Enjoy",Toast.LENGTH_SHORT).show();
+                                    d.dismiss();
+                                    un=emailEditText.getText().toString();
+                                }
+                            }
+                        });
+
+            }
+        });
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                firebaseAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passed.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("SR", "Entered onComplete");
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(),"Error - "+task.getException().getMessage().toString(),Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+
+                                    Toast.makeText(getApplicationContext(),"Yes Ok Enjoy",Toast.LENGTH_SHORT).show();
+
+
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+        d.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+
+
+
+        d.show();
+    }
 
 
 }
